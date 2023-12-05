68e56160e2485ab38f7f6be649c6be5b4a5a5599
Нормуродов Шавкат <timanormu@gmail.com>
2023-12-05 10:58:35 +0500

    news

--- pages/train/index.js
+++ pages/train/index.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 let base_url = 'http://localhost:3000/arr'
-
+let temp, um;
 let choose = 1;
 let choose_arr = [];
 let doc = document;
 let btns = doc.querySelectorAll('.header .btn');
 let p = doc.querySelector('p');
-let inputs = doc.querySelector('.inputs');
+let input = doc.querySelector('.inputs input');
 let btn = doc.querySelector('.enter');
 let view__go = doc.querySelector('.view__go');
 let view = view__go.parentElement;
-let train = doc.querySelector('.train-st')
+let train = doc.querySelector('.train-st');
+let count = doc.querySelector('.count')
 let arr;
 await axios.get(base_url)
     .then(res => {
@@ -26,7 +27,20 @@ btns.forEach(btn => {
 view__go.onclick = e => {
     view.style.display = 'none';
     train.style.display = 'block';
-
+    um = 59;
     choose_arr = arr.splice(choose - 1 * 60, choose * 60 - 1);
-    
+    repeat_rl();
+}
+function repeat_rl() {
+    let r = Math.round(Math.random() * um)
+    um--;
+    temp = choose_arr[r];
+    p.innerText = temp.trn;
+    choose_arr = choose_arr.filter(item => choose_arr.indexOf(item) !== r);
+    count.innerHTML = choose_arr.length;
+}
+btn.onclick = e => {
+    if (input.value.toLowerCase().trim() !== temp.eng.toLowerCase().trim())
+        alert("Ответ:" + temp.eng + '=' + temp.trn)
+    repeat_rl();
 }
